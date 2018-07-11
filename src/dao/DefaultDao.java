937c4ffac07a4dec853f978a233458baf661983e
adam.brzozowski <adam_19@hotmail.it>
2018-07-11 10:49:43 +0200

    MANTENZIONE DB

--- src/dao/DefaultDao.java
+++ src/dao/DefaultDao.java
@@ -1,12 +1,12 @@
 package dao;
 
 import exceptions.NoSatelliteException;
-
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DefaultDao {
-
 
     public void insertSampleData(){
 
@@ -31,17 +31,40 @@ public class DefaultDao {
             strumDao.insertStrumento("MIPS", "Spitzer", "3/7/6");
             strumDao.insertStrumento("SPIRE", "Herschel", "3/7/6");
             strumDao.insertStrumento("PACS", "Herschel", "3/7/6");
-
-
         }catch(NoSatelliteException e){
             e.printStackTrace();
         }
-
     }
 
-
     public void deleteAll() {
-
+        try {
+            Statement stmt;
+            DataSource ds = new DataSource();
+            Connection c = ds.getConnection();
+            String sql = "DELETE FROM PUNTO_SEGMENTO";
+            stmt = c.createStatement();
+            stmt.executeUpdate(sql);
+            sql = "DELETE FROM SEGMENTO";
+            stmt.executeUpdate(sql);
+            sql = "DELETE FROM PERIMETRO";
+            stmt.executeUpdate(sql);
+            sql = "DELETE FROM FILAMENTO";
+            stmt.executeUpdate(sql);
+            sql = "DELETE FROM STELLA";
+            stmt.executeUpdate(sql);
+            sql = "DELETE FROM PUNTO";
+            stmt.executeUpdate(sql);
+            sql = "DELETE FROM STRUMENTO";
+            stmt.executeUpdate(sql);
+            sql = "DELETE FROM SATELLITE";
+            stmt.executeUpdate(sql);
+            sql = "DELETE FROM UTENTE";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String args[]){
